7553728109a63eda7dac4006ded01ce23c4eeaff
barakrapat <barakrapat@gmail.com>
2021-05-23 15:39:37 +0300

    add downscale parameter - downscale the image before the stereo calculation

--- sgbm_parameters_finder.py
+++ sgbm_parameters_finder.py
@@ -16,7 +16,8 @@ class SGBMParameterFinder:
 
         self.new_size = tuple([int(x * resize_ratio) for x in self.image_l.shape[:2][::-1]])
 
-        sliders_ranges = {'minDisparity': (0, 100),
+        sliders_ranges = {'downscale rate': (1, 10),
+                          'minDisparity': (0, 100),
                           'numDisparities': (16, 32 * 16),  # must be divided by 16
                           'blockSize': (1, 31),  # odd number >= 1
                           'P1': (8, 300),  # penalty of changing 1 disparity
@@ -46,6 +47,8 @@ class SGBMParameterFinder:
 
     def add_image(self, image, new_size, sliders_num):
         image = cv2.normalize(image, None, alpha=0, beta=255, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_8U)
+        image = cv2.applyColorMap(image, cv2.COLORMAP_JET)
+
         im = Image.fromarray(cv2.resize(image, new_size))
         self.img_tk = ImageTk.PhotoImage(image=im)
         im_tk = tk.Label(self.root, image=self.img_tk)
@@ -81,12 +84,22 @@ class SGBMParameterFinder:
         params = {k: val[1].get() for k, val in self.sliders.items()}
         params['numDisparities'] = 16 * int(params['numDisparities'] / 16)
         params['blockSize'] = params['blockSize'] - params['blockSize'] % 2 + 1
+        sgbm_params = params.copy()
+        sgbm_params.pop('downscale rate')
 
-        stereo = cv2.StereoSGBM_create(**params)
-        out = stereo.compute(self.image_l, self.image_r).astype(float) / 16.
+        stereo = cv2.StereoSGBM_create(**sgbm_params)
+
+        image_l, image_r = self.downscale_images(params['downscale rate'])
+        out = stereo.compute(image_l, image_r).astype(float) / 16.
 
         self.label_ready.set('ready')
         return out
+
+    def downscale_images(self, downscale_rate):
+        resize_new_shape = tuple([int(x / downscale_rate) for x in self.image_l.shape[:2][::-1]])
+        image_l = cv2.resize(self.image_l, resize_new_shape)
+        image_r = cv2.resize(self.image_r, resize_new_shape)
+        return image_l, image_r
 
     def on_closing(self):
         if self.out_filename is not None:
